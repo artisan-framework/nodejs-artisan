27d64b4a6afd05599a5df7e8f86ee7caddf3bb16
Jeremy Mason <jeremyalan@gmail.com>
2015-12-11 07:43:42 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
+var Artisan = require('./dist/index').default;
+exports.default = Artisan;
---
+++ lib/index.ts
@@ -0,0 +1,3 @@
+var Artisan = {};
+
+export default Artisan;
